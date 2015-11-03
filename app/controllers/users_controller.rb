c2e524eb7eab0885c1830ea7cf8f1ce762378f7d
Lisa Folliard & Yelena Nedelko <student@epicodus.com>
2015-11-03 15:22:52 -0800

    add links to infowindow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
+
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
-      marker.infowindow user.address
+      marker.infowindow ("<a href='#{user.title}'>" + user.description + "</a>" )
 
+      # "<a href="user.link">user.title</a>"
     end
   end
 
