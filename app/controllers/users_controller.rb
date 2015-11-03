06e1b343b84f45a876b7b44d3450a12214920e77
Lisa Folliard & Yelena Nedelko <student@epicodus.com>
2015-11-03 11:22:31 -0800

    add info window to map

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
-end
+      marker.infowindow user.title
+    end
   end
 
   # GET /users/1
